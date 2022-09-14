05e6d06629561cafd26cf654ffdf3a09b1c2ea5f
Pedram Amani <amani.pedram@gmail.com>
2022-09-14 13:47:44 -0700

    fix Safari issues

---
+++ app/immutable/chunks/0-95883af8.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_layout.svelte-46f9fad5.js";import"./index-9b77a75e.js";import"./Image.svelte_svelte_type_style_lang-c27cfda3.js";import"./index-eebcf149.js";export{e as component};
--- app/immutable/chunks/0-d7ad8456.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/_layout.svelte-336826a8.js";import"./index-9b77a75e.js";import"./Image.svelte_svelte_type_style_lang-2e06d844.js";import"./index-eebcf149.js";export{e as component};
