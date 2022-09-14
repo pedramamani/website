77632a25106b31fd5359c3586445fd39efd44d8f
Pedram Amani <amani.pedram@gmail.com>
2022-09-14 04:50:22 -0700

    minor fixes to Safari

--- app/immutable/chunks/0-1146fdc9.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/_layout.svelte-3c83f085.js";import"./index-9b77a75e.js";import"./Image.svelte_svelte_type_style_lang-aa641385.js";import"./index-eebcf149.js";export{e as component};
---
+++ app/immutable/chunks/2-a2bce788.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_page.svelte-6614c01f.js";import"./index-9b77a75e.js";import"./Image.svelte_svelte_type_style_lang-2e06d844.js";import"./index-eebcf149.js";export{e as component};
