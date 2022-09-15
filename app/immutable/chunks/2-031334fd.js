da46c277a0a036cc067d61ddec61343536d6101c
Pedram Amani <amani.pedram@gmail.com>
2022-09-15 16:00:37 -0700

    html background + font weight

--- app/immutable/chunks/2-031334fd.js
+++
@@ -1,1 +0,0 @@
-import{default as e}from"../components/pages/_page.svelte-23d5daa9.js";import"./index-9b77a75e.js";import"./Image.svelte_svelte_type_style_lang-c27cfda3.js";import"./index-eebcf149.js";export{e as component};
---
+++ app/immutable/chunks/2-1cde5e3a.js
@@ -0,0 +1,1 @@
+import{default as e}from"../components/pages/_page.svelte-64ec0a2f.js";import"./index-9b77a75e.js";import"./Link-9970bd8e.js";import"./index-eebcf149.js";export{e as component};
