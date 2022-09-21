f5f66f2662d54cb5ed41762e26d20755194bf5d4
Pedram Amani <amani.pedram@gmail.com>
2022-09-21 03:28:12 -0700

    fix footer wrapping in firefox

--- app/immutable/chunks/1-10aec6de.js
+++
@@ -1,1 +0,0 @@
-import{default as m}from"../components/pages/_error.svelte-7a3d5979.js";import"./index-ee41e55b.js";export{m as component};
---
+++ app/immutable/chunks/2-08fde1ff.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_page.svelte-b3331d61.js";import"./index-ee41e55b.js";import"./index-dfb3dd74.js";export{r as component};
